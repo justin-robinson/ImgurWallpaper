40df76206d6fc6b2b01eda6d852fa860a016b359
jrobinson <jrob09bhs@gmail.com>
2015-10-22 17:31:21 -0400

    [master] fixed windows ssl issues

--- src/pw/jor/imgurwallpaper/Main.java
+++ src/pw/jor/imgurwallpaper/Main.java
@@ -8,12 +8,6 @@ public class Main {
 
     public static void main(String[] args) {
 
-        // make https work with windows
-        java.security.Security.addProvider(new com.sun.net.ssl.internal.ssl.Provider());
-        System.setProperty(
-                "java.protocol.handler.pkgs",
-                "com.sun.net.ssl.internal.www.protocol");
-
         // and aWAY we GO!!!
         gui = new GUI();
     }
