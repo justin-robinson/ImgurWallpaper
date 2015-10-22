e924605a4565faa874ad88a38c95b5d07e7bafe8
Justin Robinson <Justin Robinson>
2015-10-22 19:18:18 -0400

    [master] use correct url for wallpaper library sources

--- src/pw/jor/imgurwallpaper/Downloader.java
+++ src/pw/jor/imgurwallpaper/Downloader.java
@@ -17,7 +17,7 @@ import java.net.URL;
  */
 public class Downloader {
 
-    private static final String DOWNLOAD_LIST_URL = "https://jor.pw/downloads/walls.txt";
+    private static final String DOWNLOAD_LIST_URL = "https://drive.google.com/uc?export=download&id=0B72B1Sn-24fwT21DQW9fUjJacU0";
 
     /**
      * gets the contents of a url
