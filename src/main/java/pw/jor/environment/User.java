d19124ed55d3c01a5d1801314c20e6613f12f813
Justin Robinson <jrob09bhs@gmail.com>
2015-10-23 15:08:40 -0400

    [master] properly detect pictures library in windows

--- src/main/java/pw/jor/environment/User.java
+++ src/main/java/pw/jor/environment/User.java
@@ -3,45 +3,45 @@ package pw.jor.environment;
 import java.io.File;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.prefs.BackingStoreException;
-import java.util.prefs.Preferences;
 
 import org.apache.commons.lang3.SystemUtils;
+
+import com.sun.jna.platform.win32.Advapi32Util;
+import com.sun.jna.platform.win32.WinReg;
+
+
 
 /**
  * Created by jrob0 on 10/23/2015.
  */
 public class User {
-    // get user's home folder
-    String userDir = System.getenv("USERPROFILE") == null
-            ? System.getenv("HOME")
-            : System.getenv("USERPROFILE");
 
     public static Path getUserPicturesFolder () {
 
-        Path pictureFolder;
+        Path picturePath;
 
         if ( SystemUtils.IS_OS_WINDOWS ) {
-            // HKEY_CURRENT_USER/SOFTWARE/MicrosoftWindows/CurrentVersion/Explorer/User Shell Folders
-            Preferences preferences = Preferences.userRoot();
-            preferences = preferences.node("/HKCU/SOFTWARE/Microsoft/Windows/CurrentVersion/Explorer/User Shell Folders");
-            String[] chil;
-            try {
-                chil = preferences.childrenNames();
-            } catch ( BackingStoreException e ) {
+            String key = "Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\User Shell Folders";
+            String value = "My Pictures";
 
-            }
-            String p = preferences.get("My Pictures", "asdf");
-            pictureFolder = Paths.get(SystemUtils.USER_HOME, "balsssssss");
+            String pictureVariable = Advapi32Util.registryGetStringValue(
+                    WinReg.HKEY_CURRENT_USER,
+                    key,
+                    value
+            );
+
+            pictureVariable = Variable.expand(pictureVariable);
+
+            picturePath = Paths.get(pictureVariable);
 
         } else {
-            pictureFolder = Paths.get(SystemUtils.USER_HOME, "Pictures", "Wallpapers");
+            picturePath = Paths.get(SystemUtils.USER_HOME, "Pictures", "Wallpapers");
         }
 
         // make folders
-        File dir = new File(pictureFolder.toString());
+        File dir = new File(picturePath.toString());
         dir.mkdir();
 
-        return pictureFolder;
+        return picturePath;
     }
 }
